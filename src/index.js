53d70c7284e45d1432c5c5e223894101b3b3ab2c
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2021-04-30 22:32:44 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Metronome from "./components/Metronome";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDom.render(<Metronome  />, document.querySelector("#root"));
